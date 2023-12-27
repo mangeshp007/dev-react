8fb8bc1a5e5009861e18c199d594968957d5d30d
Mangesh Patil <mangesh.patil9964@gmail.com>
2023-12-27 06:53:41 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement('h1', {id:'heading',className:'Mangesh'}, "Hello React Day1!!!");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
